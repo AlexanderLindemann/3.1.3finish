87806cba4c79472f46c47f4bd28a192ad6e254ee
AlexanderLindemann <41576844+AlexanderLindemann@users.noreply.github.com>
2020-08-10 22:22:56 +0300

    Add files via upload

---
+++ 2.4.1.spring.security-SpringBoot_with_Bootstrap_3_1_2/src/main/resources/database.sql
@@ -0,0 +1,4 @@
+insert into users values (10, 165, 'admin', '$2y$12$w4Vg1IZjkCNjveT4CjHTD.EJ56W3mI0U9hXKM8jDz3rR3ZUCf49/q', 'admin', 75);
+insert into roles values (1, 'USER');
+insert into roles values(2, 'ADMIN');
+insert into user_roles values (10,2);
